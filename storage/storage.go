81a7669d06b48811f5388c35e2322081d7cfb415
relue <natalierogers048@gmail.com>
2025-06-29 15:37:51 -0400

    array of strings storage interface

--- storage/storage.go
+++ storage/storage.go
@@ -3,8 +3,8 @@ package storage
 import "time"
 
 type StorageAdapter interface {
-	Set(key string, val []byte, expiry time.Time) error
-	Get(key string) ([]byte, error)
-	Del(key string) error
-	Scan(pattern string, ch chan []byte) error
+	Set(key []string, val []byte, expiry time.Time) error
+	Get(key []string) ([]byte, error)
+	Del(key []string) error
+	Scan(pattern []string, ch chan []byte) error
 }
